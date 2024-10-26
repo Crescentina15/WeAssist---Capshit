f45975b637973a8c00a9c69946959b183d886209
Crescentina <tinayremedio26@gmail.com>
2024-10-26 10:02:21 +0800

    UI

---
+++ app/src/main/java/com/remedio/weassist/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.registration.register
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.remedio.weassist.R
+import com.remedio.weassist.registerSelection
+
+class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Using Handler to delay the transition
+        Handler().postDelayed({
+            // Start the next activity
+            val mainIntent = Intent(this, registerSelection::class.java)
+            startActivity(mainIntent)
+            finish() // Close the splash activity so the user won't go back to it
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
