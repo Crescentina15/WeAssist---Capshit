35ed2a5786cc13464d8f1374e9b7d81dca3f730d
Crescentina <tinayremedio26@gmail.com>
2025-02-09 22:14:33 +0800

    SECRETARY NI TANAN

--- app/src/main/java/com/remedio/weassist/secretaryDashboardFragment.kt
+++ app/src/main/java/com/remedio/weassist/secretaryDashboardFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [lawyersDashboardFragment.newInstance] factory method to
+ * Use the [secretaryDashboardFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class lawyersDashboardFragment : Fragment() {
+class secretaryDashboardFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class lawyersDashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lawyersdashboard, container, false)
+        return inflater.inflate(R.layout.fragment_secretarydashboard, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class lawyersDashboardFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            lawyersDashboardFragment().apply {
+            secretaryDashboardFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
