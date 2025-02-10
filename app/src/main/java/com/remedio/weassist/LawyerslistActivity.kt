e698dfc9c76ae1eb58c65939d4cd470f53f44ea1
Dominic <dominicrestauro@gmail.com>
2025-02-10 23:05:01 +0800

    added functionalities for secretary dashboard buttons

--- app/src/main/java/com/remedio/weassist/LawyerslistActivity.kt
+++ app/src/main/java/com/remedio/weassist/LawyerslistActivity.kt
@@ -11,7 +11,7 @@ class LawyerslistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lawyerslist)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lawyerlist)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
