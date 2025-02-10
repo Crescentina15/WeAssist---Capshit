3b31eff37e0a6c58b0a64e32f7de473ce87b7257
Dominic <dominicrestauro@gmail.com>
2025-02-10 14:27:26 +0800

    Fixed UI for secretary (running)

--- app/src/main/java/com/remedio/weassist/GiveratingsActivity.kt
+++ app/src/main/java/com/remedio/weassist/GiveratingsActivity.kt
@@ -1,6 +1,8 @@
 package com.remedio.weassist
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,16 @@ class GiveratingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_giveratings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val mainLayout = findViewById<View>(R.id.mainLayout)  // Use correct ID
+        if (mainLayout != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        } else {
+            Log.e("GiveratingsActivity", "mainLayout is null!")
         }
     }
 }
